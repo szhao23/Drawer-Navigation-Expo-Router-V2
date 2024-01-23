61d920008efccaba620b2aaa02d41f7db4e0c245
Steven <65731775+szhao23@users.noreply.github.com>
2024-01-23 13:48:52 -0600

    Added Ionicons for Navigation Menu options and adjusted CSS Styling

--- code/Expo-Drawer/app/_layout.tsx
+++ code/Expo-Drawer/app/_layout.tsx
@@ -1,19 +1,30 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Drawer } from "expo-router/drawer";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 // Default Layout Component
 
 // This file will add a header to our application which contains our Drawer Menu Navigation
 export default function _layout() {
   return (
-    <Drawer>
+    // Modifies the CSS Styling and Spacing for Nav Header/Title
+    <Drawer
+      screenOptions={{
+        drawerLabelStyle: {
+          marginLeft: -20,
+        },
+      }}
+    >
       {/* This will change the header title from index to whatever we want, in this case changed it to About */}
       <Drawer.Screen
         name="home"
         options={{
           drawerLabel: "Home",
           title: "Home",
+          drawerIcon: ({ size, color }) => (
+            <Ionicons name="home-sharp" size={size} color={color} />
+          ),
         }}
       />
       <Drawer.Screen
@@ -21,6 +32,13 @@ export default function _layout() {
         options={{
           drawerLabel: "About",
           title: "About",
+          drawerIcon: ({ size, color }) => (
+            <Ionicons
+              name="information-circle-sharp"
+              size={size}
+              color={color}
+            />
+          ),
         }}
       />
     </Drawer>
