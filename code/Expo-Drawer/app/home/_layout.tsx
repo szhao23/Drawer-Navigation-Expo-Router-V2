841280d3449b30a25e73d89235d9281af13a7279
Steven <65731775+szhao23@users.noreply.github.com>
2024-01-23 13:54:01 -0600

    Added Settings and Notifications Icons for Tabs

--- code/Expo-Drawer/app/home/_layout.tsx
+++ code/Expo-Drawer/app/home/_layout.tsx
@@ -1,6 +1,7 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 // Within Home Component, adding the layout and tabs helps keep things organized
 export default function _layout() {
@@ -10,6 +11,25 @@ export default function _layout() {
       screenOptions={{
         headerShown: false,
       }}
-    ></Tabs>
+    >
+      {/* Add Settings Icon for "settings" tab */}
+      <Tabs.Screen
+        name="settings"
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="settings-sharp" size={size} color={color} />
+          ),
+        }}
+      />
+      {/* Add Notifications Icon for "notifications" tab */}
+      <Tabs.Screen
+        name="notifications"
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="notifications" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
